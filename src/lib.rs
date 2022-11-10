b3d49f65187cd8dd117243672fca205e66013275
sin <wiavyun123@gmail.com>
2022-11-11 04:07:39 +0800

    Optimize text splicing, hand over IO operations to users, and prepare to support WASM and input generic support

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
 //! A rust implementation of China's standards of encryption algorithms(SM2/SM3/SM4).
-//! 
+//!
 //! ## Quick Start
-//! 
+//!
 //! ### SM3
-//! 
+//!
 //! ```
 //! let hash = sm3::sm3_hash(b"abc");
 //! assert_eq!(hash, "66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0");
 //! ```
-//! 
+//!
 //! ### SM2 Key Generate
-//! 
+//!
 //! Note that the public key is in hexadecimal format and does not contain the "04" prefix.
-//! 
+//!
 //! ```
 //! let (sk, pk) = sm2::gen_keypair();
 //! ```
-//! 
+//!
 //! ### SM2 Sign/Verify
-//! 
+//!
 //! ```
 //! let sign_ctx = sm2::Sign::new(&sk);
 //! let sign = sign_ctx.sign(b"abc");
@@ -26,9 +26,9 @@
 //! let verify = verify_ctx.verify(b"abc", &sign);
 //! assert_eq!(verify, true);
 //! ```
-//! 
+//!
 //! ### SM2 Encrypt/Decrypt
-//! 
+//!
 //! ```
 //! let enc_ctx = sm2::Encrypt::new(&pk);
 //! let enc = enc_ctx.encrypt(b"abc");
@@ -36,9 +36,9 @@
 //! let dec = dec_ctx.decrypt(&enc);
 //! assert_eq!(String::from_utf8(dec).unwrap(), "abc");
 //! ```
-//! 
+//!
 //! ### SM2 Key Exchange
-//! 
+//!
 //! ```
 //! // Step 1
 //! // a side
@@ -60,9 +60,9 @@
 //! assert_eq!(ka.k, kb.k);
 //! assert_eq!(ka.s12, kb.s12);
 //! ```
-//! 
+//!
 //! ### SM4 ECB Encrypt/Decrypt
-//! 
+//!
 //! ```
 //! let key = b"1234567812345678";
 //! let sm4_ecb = sm4::CryptSM4ECB::new(key);
@@ -70,9 +70,9 @@
 //! let dec_ecb = sm4_ecb.decrypt_ecb(&enc_ecb);
 //! assert_eq!(String::from_utf8(dec_ecb).unwrap(), "abc");
 //! ```
-//! 
+//!
 //! ### SM4 CBC Encrypt/Decrypt
-//! 
+//!
 //! ```
 //! let key = b"1234567812345678";
 //! let iv = b"0000000000000000";
@@ -85,3 +85,22 @@
 pub mod sm2;
 pub mod sm3;
 pub mod sm4;
+
+#[test]
+fn test_sm() {
+    let hash = sm3::sm3_hash(b"abc");
+    assert_eq!(
+        hash,
+        "66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0"
+    );
+
+    let (sk, pk) = sm2::gen_keypair();
+
+    let enc_ctx = sm2::Encrypt::new(&pk);
+    let enc = enc_ctx.encrypt(b"abc");
+
+    let dec_ctx = sm2::Decrypt::new(&sk);
+    let dec = dec_ctx.decrypt(&enc);
+    let code = String::from_utf8(dec).unwrap();
+    assert_eq!(code, "abc");
+}
